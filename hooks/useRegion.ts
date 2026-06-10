9c1c76d5f8e637838de58db35e99c93136decb87
chirag <chiragmiglani73@gmail.com>
2026-06-10 22:32:02 +0530

    errors fixed

--- hooks/useRegion.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useRegionContext } from "@/components/RegionProvider";
-
-export function useRegion() {
-  return useRegionContext();
-}
--- lib/hreflang.ts
+++ lib/hreflang.ts
@@ -1,28 +1,11 @@
-import {
-  REGIONS,
-  RegionConfig,
-} from "@talentdash/types";
+export type RegionConfig = {
+  code: string;
+  defaultLanguage: string;
+};
 
-const BASE_URL = "https://talentdash.com";
-
-export function generateHreflangLinks(
-  pathname: string,
-) {
-  return Object.values(REGIONS).map(
-    (region: RegionConfig) => {
-      const href =
-        region.code === "global"
-          ? `${BASE_URL}${pathname}`
-          : `${BASE_URL}/${region.code}${pathname}`;
-
-      return {
-        rel: "alternate",
-        hrefLang:
-          region.code === "global"
-            ? "x-default"
-            : region.defaultLanguage,
-        href,
-      };
-    },
-  );
-}
+export const REGIONS = {
+  global: {
+    code: "global",
+    defaultLanguage: "en",
+  },
+};
